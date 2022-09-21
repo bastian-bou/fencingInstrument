77a7f5beacc38e8d651e565e5e9fe904a9d02191
bastian-bou <bastian.bouchardon@gmail.com>
2022-09-22 00:02:55 +0200

    Change Licence, add midi, micro rules to compile, everything ...

--- fencingInstrument/include/main.h
+++ fencingInstrument/include/main.h
@@ -1,5 +1,15 @@
+/**
+ * @file main.h
+ * @author B. Bouchardon
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-21
+ * 
+ */
+
 #include <Arduino.h>
 #include "TFMiniS.h"
+#include <SoftwareSerial.h>
 
 #ifndef NO_MIDI
 #include "MIDIUSB.h"
@@ -9,7 +19,7 @@
 #define DURATION_ROW 4
 #define NUM_NOTES 14
 
-#define MAX_LENGHT_CM 14000
+#define MAX_LENGHT_CM 1400
 
 enum error_type {
     LIDAR_TEMP_TOO_HIGH = 0,
@@ -77,6 +87,7 @@ struct LedStatus {
 };
 
 #ifndef NO_MIDI
+// list of notes to play at each meter
 const byte notePitches[NUM_NOTES] = {pitchC3, pitchD3, pitchE3, pitchF3, pitchG3, pitchA3, pitchB3, pitchC4, pitchD4, pitchE4, pitchF4, pitchG4, pitchA4, pitchB4};
 #endif
 
@@ -84,5 +95,11 @@ Measurement measurement;
 uint16_t last_distance;
 uint16_t offset_position;
 
+/**
+ * @brief Set a MIDI note
+ */
 void noteOn(byte channel, byte pitch, byte velocity);
+/**
+ * @brief Unset a MIDI note
+ */
 void noteOff(byte channel, byte pitch, byte velocity);
--- fencingInstrument/src/main.cpp
+++ fencingInstrument/src/main.cpp
@@ -1,39 +1,50 @@
+/**
+ * @file main.cpp
+ * @author B. Bouchardon
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-21
+ * 
+ */
 #include "main.h"
-#include <SoftwareSerial.h>
 
-#define FRAME_RATE_MS 25
-#define MIN_STRENGHT 500
-// Possible values : 0 - 127
-#define MIDI_VELOCITY 127
+/** Time between two measures (ms) */
+#define FRAME_RATE_MS 100
+/** Midi channel to communicate on USB */
 #define MIDI_CHANNEL 0
-#define HYST_CM 5
+/** Distance hysteresis (cm) */
+#define HYST_CM 10
+/** Offset position (minimun and maximum distance) in cm */
+#define OFFSET_POS 30
 
-
-// Setup software serial port 
-SoftwareSerial mySerial(10, 11);      // Uno RX (TFMINI TX), Uno TX (TFMINI RX)
+// Lidar sensor object
 TFMiniS tfmini;
+/** 
+ * Serial interface object
+ * 10 = RX, 11 = TX
+ */
+SoftwareSerial mySerial(10, 11);
 
-LedStatus ledBoard(LED_BUILTIN);
+//LedStatus ledBoard(LED_BUILTIN);
 
 void setup()
 {
+  // Initialise structure
   measurement.distance = 0;
   measurement.strength = 0;
   measurement.temperature = 0;
 
   last_distance = 0;
 
-  offset_position = 30;
-
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 #ifdef DEBUG
   // Step 1: Initialize hardware serial port (serial debug port)
-  Serial.begin(115200);
+  Serial.begin(9600);
   // wait for serial port to connect. Needed for native USB port only
   while (!Serial);
      
-  Serial.println ("Initializing...");
+  Serial.println("Initializing...");
 
 #endif
 
@@ -43,15 +54,9 @@ void setup()
   // Step 3: Initialize the TF Mini sensor
   tfmini.begin(&mySerial);
 
-  // Step 3: Set the frame rate to how quickly you are planning on reading from the device
-  // Picking your frame rate is the most imporant part because UART will buffer readings and
-  // if you aren't reading quickly enough then the sensor values will change VERY slowly.
-  // You'll notice that I've set the frame rate to 4Hz and in the loop I'm delaying by 250ms
-  // So I will be keeping up with the sensor readings. This is what makes the sensor seem
-  // performant.
-  tfmini.setFrameRate(FRAME_RATE_MS);
+  // 10Hz to refesh value
+  tfmini.setFrameRate(10);
 
-  // Step 4: Save setting. This is not necessary since we are setting the frame rate on every boot.
   tfmini.saveSettings();
 }
 
@@ -67,12 +72,20 @@ void loop()
   {
     lastMillis = currentMillis;
     measurement = tfmini.getMeasurement();
+    #ifdef DEBUG
+      Serial.print("Dist: ");
+      Serial.println(measurement.distance);
+      /*Serial.print(" Strength: ");
+      Serial.print(measurement.strength);
+      Serial.print(" Temperature: ");
+      Serial.println(measurement.temperature);*/
+    #endif
   }
 
   if (measurement.distance != last_distance)
   {
-    if ((measurement.distance >= offset_position) and
-        (measurement.distance <= MAX_LENGHT_CM + offset_position))
+    if ((measurement.distance >= OFFSET_POS) and
+        (measurement.distance <= MAX_LENGHT_CM + OFFSET_POS))
     {
       if (((measurement.distance - HYST_CM) >= last_distance) or
           ((measurement.distance + HYST_CM) <= last_distance))
@@ -93,6 +106,7 @@ void loop()
         if (last_position_note != new_position_note) {
           noteOff(MIDI_CHANNEL, notePitches[last_position_note], 0);
         }
+        // Send MIDI data to USB
         MidiUSB.flush();
       #endif
         last_distance = measurement.distance;
@@ -106,24 +120,29 @@ void loop()
     ledBoard.setRowDurationError(error_type::NO_ERROR);
   }*/
 
-#ifdef DEBUG
-  Serial.print("Dist: ");
-  Serial.print(measurement.distance);
-  Serial.print(" Strength: ");
-  Serial.print(measurement.strength);
-  Serial.print(" Temperature: ");
-  Serial.println(measurement.temperature);
-#endif
-
 }
 
 #ifndef NO_MIDI
+/**
+ * @brief Set a MIDI note
+ * 
+ * @param channel channel number to communicate
+ * @param pitch note to play
+ * @param velocity the force with which you press the note
+ */
 void noteOn(byte channel, byte pitch, byte velocity)
 {
   midiEventPacket_t noteOn = {0x09, (uint8_t)(0x90 | channel), pitch, velocity};
   MidiUSB.sendMIDI(noteOn);
 }
 
+/**
+ * @brief Unset a MIDI note
+ * 
+ * @param channel channel number to communicate
+ * @param pitch note to unplay
+ * @param velocity the force with which you press the note (not mandatory)
+ */
 void noteOff(byte channel, byte pitch, byte velocity)
 {
   midiEventPacket_t noteOff = {0x08, (uint8_t)(0x80 | channel), pitch, velocity};
